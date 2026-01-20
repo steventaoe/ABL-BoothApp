b3fd24a59dbb5a7cc072b84753795fd608de3192
Renko_6626 <108311284+Renko6626@users.noreply.github.com>
2026-01-20 12:45:56 +0800

    fixed a name error

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
 import { defineClientComponent } from 'vitepress'
-import Landing from './components/landing.vue'
+import Landing from './components/Landing.vue'
 import Timeline from './components/Timeline.vue'
 export default {
   extends: DefaultTheme,
